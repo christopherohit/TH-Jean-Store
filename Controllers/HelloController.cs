541015357bc15f7c67862579b4321c5b6eccf269
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-05-08 09:32:47 +0700

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEmpty.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Hello()
+        {
+            return View();
+        }
+        public ActionResult SayHello()
+        {
+            return Content("Welcome to My site");
+        }
+    }
+}
